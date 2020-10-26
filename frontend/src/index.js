55c86689b0dbab20a7482b59ecff93ea5f33e15a
anair22 <aishwarya.nair22@gmail.com>
2020-10-26 01:03:25 -0700

    Add login/signup/tweets frontend functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import axios from 'axios';
 import Root from './components/root';
 import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
